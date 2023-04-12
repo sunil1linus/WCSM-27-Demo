d5486d0bbe6fcc34988235362cee2c62b4186d95
sunil <sunil@LAPTOP-ATHU9U5K>
2023-04-12 11:16:40 +0530

    commited1

--- src/test/java/practice/Sample.java
+++ src/test/java/practice/Sample.java
@@ -1,9 +1,13 @@
 package practice;
 
+import org.testng.annotations.Test;
+
 public class Sample {
 	
+	@Test
 	public void sample() {
 		System.out.println("SAMPLE");
+		System.out.println("hi");
 		
 	}
 
